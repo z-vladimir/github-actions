e5942e67291066259b11670810dd40f316855487
Vladimir <vladimir@MacBook-Air-Vladimir.local>
2023-05-25 13:10:35 +0300

    Add workflows

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 
+// Commit
+
 function App() {
   return (
     <div className="App">
@@ -15,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React!!!
         </a>
       </header>
     </div>
